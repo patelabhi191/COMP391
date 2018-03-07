c9f5075f882d9478b6bc1735477e5cdf64e74351
Abhi Patel <patelabhi191@gmail.com>
2018-03-07 14:13:26 -0500

    [ADD] Asteroid and blasting effects

---
+++ COMP 391/Assets/Scripts/BoundaryDestroyer.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoundaryDestroyer : MonoBehaviour {
+
+	public GameObject ExplosionAsteroid;
+
+	// Use this when objects enter collider
+	void OnTriggerEnter2D (Collider2D other) 
+	{
+		Destroy (this.gameObject);	
+	}
+	
+/*	// Runs when object is inside the box
+	void OnTriggerStay2D (Collider2D other) 
+	{
+
+	}
+
+	//Runs when object exits the collider box
+	void OnTriggerExit2D (Collider2D other) 
+	{
+
+	}*/
+}
---
+++ COMP 391/Assets/Scripts/RandomRotater.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomRotater : MonoBehaviour {
+
+	public float tumble;
+
+	private Rigidbody2D rb;
+
+	// Use this for initialization
+	void Start () 
+	{
+		rb = GetComponent<Rigidbody2D> ();
+		rb.angularVelocity = Random.value * tumble;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
