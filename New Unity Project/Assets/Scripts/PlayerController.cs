90d459f60ab09cf849bf1b1bf671a3bec26f2427
Abhi Patel <300984015@cencol.ca>
2018-06-28 16:12:59 -0400

    [Add] some animations.

---
+++ New Unity Project/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+	public float speed = 5;
+
+	private Rigidbody2D rBody;
+	private Animator animator;
+
+	// Use this for initialization
+	void Start () {
+
+		rBody = GetComponent<Rigidbody2D>;
+		animator = GetComponent<Animator>;
+		
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+
+		float moveH = Input.GetAxis ("Horizontal");
+
+		animator.SetFloat ("hSpeed", Mathf.Abs(moveH));
+
+		rBody.velocity = new Vector2 (moveH, rBody.velocity.y);
+		
+	}
+}
