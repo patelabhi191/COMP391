d000875fbd3744fbe210b72af2c051d59195e39b
Abhi Patel <300984015@cencol.ca>
2018-07-10 12:16:10 -0400

    [Add] jump and movement

--- New Unity Project/Assets/Scripts/PlayerController.cs
+++ New Unity Project/Assets/Scripts/PlayerController.cs
@@ -4,27 +4,55 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
 
-	public float speed = 5;
+    [Header("Movement Settings")]
+    public float jumpForce;
+	public float speed;
 
-	private Rigidbody2D rBody;
+    [Header("Ground Check")]
+    public LayerMask defineGround;
+    
+
+    private Rigidbody2D rBody;
 	private Animator animator;
+    private float disToGround;
+    private bool isRight = true;
+    private bool isGrounded = false;
 
 	// Use this for initialization
 	void Start () {
 
-		rBody = GetComponent<Rigidbody2D>;
-		animator = GetComponent<Animator>;
+		rBody = GetComponent<Rigidbody2D>();
+		animator = GetComponent<Animator>();
+
+        disToGround = GetComponent<Collider2D>().bounds.extents.y;
 		
 	}
 	
 	// Update is called once per frame
 	void FixedUpdate () {
 
+        isGrounded = CheckIsGround();
+        animator.SetBool("isGrounded");
+
+        if (isGrounded && Input.GetAxis("Jump") > 0)
+        {
+            rBody.AddForce(new Vector2(0, jumpForce), ForceMode2D.Force);
+            isGrounded = false;
+        }
+
 		float moveH = Input.GetAxis ("Horizontal");
 
-		animator.SetFloat ("hSpeed", Mathf.Abs(moveH));
+		animator.SetFloat ("hSpeed", Mathf.Abs(moveH) * speed);
 
 		rBody.velocity = new Vector2 (moveH, rBody.velocity.y);
+
 		
 	}
+
+    private bool CheckIsGround()
+    {
+        return Physics2D.Raycast(transform.position, Vector3.down, disToGround + 0.1f, defineGround);
+    }
+
+    
 }
